a70ffd13410eef2f008b113ae98b3d50524e5322
keerthanauppala <keerthanauppala40@gmail.com>
2020-12-01 11:47:05 -0500

    Updated student.java and other files

--- swe642-hw4-restful/src/main/java/org/gmu/swe642/HW4/repository/StudentRepo.java
+++ swe642-hw4-restful/src/main/java/org/gmu/swe642/HW4/repository/StudentRepo.java
@@ -4,5 +4,5 @@ import org.gmu.swe642.HW4.domain.Student;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepo extends CrudRepository<Student, Long> {
-    Student findByStdid(String stdid);
+    Student findByStdId(String stdid);
 }
